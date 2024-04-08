94aebddc5392366a3d9be525117a98c99125f52e
amnasohaib <97840320+amnasohaib@users.noreply.github.com>
2024-04-08 05:49:54 +0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,79 @@
+function runAnimations() {
+  runAnimations_693();
+}
+runAnimations();
+function runAnimations_693() {
+  setTimeout(() => {
+    document.querySelector("#id-684").classList.add("animationClass-684");
+    document
+      .querySelector(".i-am-a-1-127236-0")
+      .classList.add("animationClass-627-627-0");
+    document.querySelector("#id-627").classList.add("animationClass-627");
+    document.querySelector("#id-657").classList.add("animationClass-657");
+    document
+      .querySelector(".student-program-1-588720-0")
+      .classList.add("animationClass-629-629-0");
+    document.querySelector("#id-629").classList.add("animationClass-629");
+  }, 800);
+
+  setTimeout(() => {
+    document.querySelector("#id-684").classList.add("animationClass-682");
+    document
+      .querySelector(".i-am-a-1-127236-0")
+      .classList.add("animationClass-660-660-0");
+    document.querySelector("#id-627").classList.add("animationClass-660");
+    document.querySelector("#id-657").classList.add("animationClass-661");
+    document
+      .querySelector(".student-program-1-588720-0")
+      .classList.add("animationClass-662-662-0");
+    document.querySelector("#id-629").classList.add("animationClass-662");
+  }, 2200);
+
+  setTimeout(() => {
+    document.querySelector("#id-684").classList.add("animationClass-683");
+    document
+      .querySelector(".i-am-a-1-127236-0")
+      .classList.add("animationClass-664-664-0");
+    document.querySelector("#id-627").classList.add("animationClass-664");
+    document.querySelector("#id-657").classList.add("animationClass-665");
+    document
+      .querySelector(".student-program-1-588720-0")
+      .classList.add("animationClass-666-666-0");
+    document.querySelector("#id-629").classList.add("animationClass-666");
+
+    setTimeout(() => {
+      //loop login
+      document.querySelector("#id-684").classList.remove("animationClass-684");
+      document
+        .querySelector(".i-am-a-1-127236-0")
+        .classList.remove("animationClass-627-627-0");
+      document.querySelector("#id-627").classList.remove("animationClass-627");
+      document.querySelector("#id-657").classList.remove("animationClass-657");
+      document
+        .querySelector(".student-program-1-588720-0")
+        .classList.remove("animationClass-629-629-0");
+      document.querySelector("#id-629").classList.remove("animationClass-629");
+      document.querySelector("#id-684").classList.remove("animationClass-682");
+      document
+        .querySelector(".i-am-a-1-127236-0")
+        .classList.remove("animationClass-660-660-0");
+      document.querySelector("#id-627").classList.remove("animationClass-660");
+      document.querySelector("#id-657").classList.remove("animationClass-661");
+      document
+        .querySelector(".student-program-1-588720-0")
+        .classList.remove("animationClass-662-662-0");
+      document.querySelector("#id-629").classList.remove("animationClass-662");
+      document.querySelector("#id-684").classList.remove("animationClass-683");
+      document
+        .querySelector(".i-am-a-1-127236-0")
+        .classList.remove("animationClass-664-664-0");
+      document.querySelector("#id-627").classList.remove("animationClass-664");
+      document.querySelector("#id-657").classList.remove("animationClass-665");
+      document
+        .querySelector(".student-program-1-588720-0")
+        .classList.remove("animationClass-666-666-0");
+      document.querySelector("#id-629").classList.remove("animationClass-666");
+      runAnimations_693();
+    }, 600);
+  }, 3599.9999999999995);
+}
